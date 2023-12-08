580ab06422000e3070260a9258fd4520963ceebe
Yuxiao Zhang <zyxdream6217@gmail.com>
2023-12-08 13:37:30 -0800

    modified function for app use

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from rewritter import ResumeQueryGenerator, ResumeGenerator
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+load_dotenv(dotenv_path=os.path.join(os.getcwd(), ".env"))
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+print("here")
 
 app = Flask(__name__)
 CORS(app)
@@ -9,9 +18,12 @@ def morph():
     data = request.json
     input1 = data.get('input1', '')
     input2 = data.get('input2', '')
-    
+    print("hi, type of input2 is ", type(input2))
+
     # TODO: add the openAI call here
-    result = input1 + " " + input2
+
+    resume_query = ResumeQueryGenerator(resume=input1, job=input2)
+    result = ResumeGenerator(resume_query)
     # print('sending back :', result)
     return jsonify({"result": result})
 
--- rewritter.py
+++ rewritter.py
@@ -17,46 +17,42 @@ load_dotenv(dotenv_path=os.path.join(os.getcwd(), ".env"))
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 print("here")
 
-def QueryGenerator(resume, jobs):
-    queries = []
-    for idx, job in enumerate(jobs):
-        jobId = job["job_id"]
-        print(jobId)
-        jobContent = job["content"]
-        query = {"job_id": jobId,
-                 "resume_prompt": "Rewrite my resume tailored to the following specific job_posting. Try to repharase my experiences, projects and eductation that are most relevant to the job descriptions and responsibilities to make my resume look more competitive. Narrow down history to only list relevant roles or experiences. Format my resume so that it is easy to identify my qualifications for this specific job. \n === my resume: {} ====\n === job posting: {}\n === the rewritten resume :".format(
-                     resume, jobContent),
-                 "letter_prompt": "A cover letter is a one-page document you send with your resume that provides additional information about skills and experiences related to the job you're pursuing. It typically includes three to four short paragraphs. A cover letter is important because it serves as the first chance for the recruiter to see the qualifications that make you a good fit for the position. Write a cover cover for the following job, based on the following resume. === job posting : {}\n ==== my resume: {} =====\n the cover letter :".format(
-                     jobContent, resume)}
+# combine resume and job posting to generating queries for rewriting resume
+def ResumeQueryGenerator(resume, job):
+    jobId = job["job_id"]
+    print(jobId)
+    jobContent = job["content"]
+    query = {"job_id": jobId,
+             "prompt": "Rewrite my resume tailored to the following specific job_posting. Try to repharase my experiences, projects and eductation that are most relevant to the job descriptions and responsibilities to make my resume look more competitive. Narrow down history to only list relevant roles or experiences. Format my resume so that it is easy to identify my qualifications for this specific job. \n === my resume: {} ====\n === job posting: {}\n === the rewritten resume :".format(
+                 resume, jobContent)}
+    return query
 
-        queries.append(query)
-    return queries
+# combine resume and job posting to generating queries for writing cover letters
+def LetterQueryGenerator(resume, job):
+    jobId = job["job_id"]
+    print(jobId)
+    jobContent = job["content"]
+    query = {"job_id": jobId,
+             "prompt": "A cover letter is a one-page document you send with your resume that provides additional information about skills and experiences related to the job you're pursuing. It typically includes three to four short paragraphs. A cover letter is important because it serves as the first chance for the recruiter to see the qualifications that make you a good fit for the position. Write a cover cover for the following job, based on the following resume. === job posting : {}\n ==== my resume: {} =====\n the cover letter :".format(
+                 jobContent, resume)}
+    return query
 
-def ResponseGenerator(query, out_dir):
-    id = query["job_id"]
+def ResumeGenerator(resume_query):
+    id = resume_query["job_id"]
     print("job ", id)
-    resume_prompt = query["resume_prompt"]
+    resume_prompt = resume_query["prompt"]
     # print("=== resume prompty ===\n", resume_prompt)
-    letter_prompt = query["letter_prompt"]
-    # print("=== letter prompt ===\n", letter_prompt)
-
     completion = client.completions.create(model="gpt-3.5-turbo-instruct", prompt=resume_prompt, max_tokens=1024)
-    response = completion.choices[0].text
-    print(f'resume-response: {response}')
-    print("==== ====")
-    filename = out_dir + "resume-" + str(id)
-    print(filename)
-    with open(filename, 'w') as outfile:
-        outfile.write(response)
+    return completion.choices[0].text
 
+def CoverLetterGenerator(letter_query):
+    id = letter_query["job_id"]
+    print("job ", id)
+    letter_prompt = letter_query["prompt"]
+    # print("=== letter prompt ===\n", letter_prompt)
     completion = client.completions.create(model="gpt-3.5-turbo-instruct", prompt=letter_prompt, max_tokens=1024)
     response = completion.choices[0].text
-    print(f'letter-response: {response}')
-    print("==== END OF RESPONSE ====")
-    filename = out_dir + "letter-" + str(id)
-    print(filename)
-    with open(filename, 'w') as outfile:
-        outfile.write(response)
+    return response
 
 
 if __name__ == "__main__":
@@ -76,14 +72,23 @@ if __name__ == "__main__":
         jobs.append({"job_id":file,"content":content})
     print(len(jobs))
 
-    # combine resume and job posting to generating queries for rewriting resume and writing cover letters
-    queries = QueryGenerator(resume, jobs)
-    print(len(queries))
     # let llm complete response and save
     out_dir = "./output/"
-    for query in queries:
-        print(query["job_id"])
-        ResponseGenerator(query, out_dir)
+    for job in jobs:
+        jobId = job["job_id"]
+        task = "resume"
+        resume_query = ResumeQueryGenerator(resume, job)
+        new_resume = ResumeGenerator(resume_query)
 
+        filename = out_dir + task + "-" + str(jobId)
+        print(filename)
+        with open(filename, 'w') as outfile:
+            outfile.write(new_resume)
 
-
+        task = "letter"
+        letter_query = LetterQueryGenerator(resume, job)
+        cover_letter = CoverLetterGenerator(letter_query)
+        filename = out_dir + task + "-" + str(jobId)
+        print(filename)
+        with open(filename, 'w') as outfile:
+            outfile.write(cover_letter)
